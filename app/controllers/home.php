cf47b0a8b01de9cc56765a98c8d4fba53d327835
BuildTools <unconfigured@null.spigotmc.org>
2018-11-01 23:05:29 +0100

    Funtionality

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -9,6 +9,19 @@
 class home extends Controller
 {
     public function index($param1, $param2) {
-        echo "HELLO";
+        $user = $this->model('User');
+        $user->name = $param1;
+
+        $this->view('home/index', $this->getData());
+    }
+
+    public function getData(){
+        $dummyData = (object) [];
+
+        $dummyData->name = "Test";
+
+        $dummyData->desc = "Hello there";
+
+        return $dummyData;
     }
 }
