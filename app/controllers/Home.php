14dbf635177ba44625eea29e46612a956a3fbf28
BuildTools <unconfigured@null.spigotmc.org>
2018-11-01 22:11:38 +0100

    More functionality

---
+++ app/controllers/Home.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Niclas
+ * Date: 01-11-2018
+ * Time: 21:50
+ */
+
+class Home extends Controller
+{
+    public function index($param1, $param2) {
+
+    }
+}
---
+++ app/models/User.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Niclas
+ * Date: 01-11-2018
+ * Time: 22:10
+ */
+
+class User
+{
+    public $name;
+}
